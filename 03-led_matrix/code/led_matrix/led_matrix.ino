97acf43f19bf1b5680007f12e40018ac41614777
Scrocetti <sofia.17.crocetti@gmail.com>
2025-06-19 21:38:16 -0300

    FEAT modify the code for the led matrix project

--- 03-led_matrix/code/led_matrix/led_matrix.ino
+++ 03-led_matrix/code/led_matrix/led_matrix.ino
@@ -1,4 +1,9 @@
-const long delay_time=250;
+const bool on = false;
+const long delay_time = 100;
+const int filablanca1 = 2;
+const int filablanca2 = 3;
+const int filanegra1 = 8;
+const int filanegra2 = 9;
 void setup() {
   // put your setup code here, to run once:
   for(int i=2;i<10;i++){
@@ -8,10 +13,23 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i=2;i<10;i++){
-    digitalWrite(i, HIGH);
+  if (on){
+    for(int i=2;i<10;i++){
+      digitalWrite(i, HIGH);
+      if(i==filablanca1 || i==filablanca2 || i==filanegra1 || i==filanegra2){
+        continue;
+      }
+      delay(delay_time);
+      digitalWrite(i, LOW);
+      delay(delay_time);
+    }
     delay(delay_time);
-    digitalWrite(i, LOW);
+
+    for(int i=2;i<10;i++){
+      if(i==filablanca1 || i==filablanca2 || i==filanegra1 || i==filanegra2){
+        digitalWrite(i, LOW);
+      }
+    }
     delay(delay_time);
   }
 }
