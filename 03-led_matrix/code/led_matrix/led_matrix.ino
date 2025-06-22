9925a6783878edac4cb6b02b3128b1ef53e5406a
Scrocetti <sofia.17.crocetti@gmail.com>
2025-06-21 21:20:09 -0300

    FEAT add comments to the code for the arduino

--- 03-led_matrix/code/led_matrix/led_matrix.ino
+++ 03-led_matrix/code/led_matrix/led_matrix.ino
@@ -1,17 +1,21 @@
 const bool on = true;
 const long delay_time = 300;
 void setup() {
-  // put your setup code here, to run once:
+  // Set all pins conected to the led matrix to output
+  // we only controll rows 2 to 7 and columns 2 to 7
   for(int i=2;i<14;i++){
     pinMode(i, OUTPUT);
   }
+  // Set all catode (odd) pines to high so those leds wont fire whe their row is on high
   for(int i=3;i<14;i+=2){
     digitalWrite(i,HIGH);
   }
 }
 
 void loop() {
+  // even pins are anode for the leds witch are the rows of the matrix
   for(int i=2;i<13;i+=2){
+    // odd pins are catode for the leds witch are the columns of the matrix
     for(int j=3;j<14;j+=2){
       digitalWrite(i,HIGH);
       digitalWrite(j,LOW);
