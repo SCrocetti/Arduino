dc24ed0c65221db253f92146ac1c933bf9b4e7de
Scrocetti <sofia.17.crocetti@gmail.com>
2025-06-23 23:30:47 -0300

    FEAT put the duration of the sweep in ms and create a function to draw words

--- projects/05-led_matrix_sweep/code/led_matrix_sweep/led_matrix_sweep.ino
+++ projects/05-led_matrix_sweep/code/led_matrix_sweep/led_matrix_sweep.ino
@@ -1,6 +1,5 @@
-const bool on = true;
-const long delay_time = 500;
-const int sweeps=400;
+const long delay_time = 250;
+const int duration=1500;
 // represents the position of each led catode and anode
 // [0][1][0] row 0 column 1 , catode
 // [0][1][1] row 0 column 1 , anode
@@ -12,7 +11,78 @@ const int transition_matrix[6][6][2]={
   {{10,3},{10,5},{10,7},{10,9},{10,11},{10,13}},
   {{12,3},{12,5},{12,7},{12,9},{12,11},{12,13}}
 };
+
+bool m_matrix[6][6]={
+  {true,false,false,false,false,true},
+  {true,true,false,false,true,true},
+  {true,false,true,true,false,true},
+  {true,false,true,true,false,true},
+  {true,false,false,false,false,true},
+  {true,false,false,false,false,true}
+};
+
+bool i_matrix[6][6]={
+  {false,true,true,true,true,false},
+  {false,false,true,true,false,false},
+  {false,false,true,true,false,false},
+  {false,false,true,true,false,false},
+  {false,false,true,true,false,false},
+  {false,true,true,true,true,false}
+};
+
+bool a_matrix[6][6]={
+  {true,true,true,true,true,true},
+  {true,true,false,false,true,true},
+  {true,true,false,false,true,true},
+  {true,true,true,true,true,true},
+  {true,true,false,false,true,true},
+  {true,true,false,false,true,true}
+  
+};
+
+bool u_matrix[6][6]={
+  {true,true,false,false,true,true},
+  {true,true,false,false,true,true},
+  {true,true,false,false,true,true},
+  {true,true,false,false,true,true},
+  {true,true,true,true,true,true},
+  {true,true,true,true,true,true}
+};
+
+bool o_matrix[6][6]={
+  {false,true,true,true,true,false},
+  {true,true,true,true,true,true},
+  {true,true,false,false,true,true},
+  {true,true,false,false,true,true},
+  {true,true,true,true,true,true},
+  {false,true,true,true,true,false}
+};
+
+bool r_matrix[6][6]={
+  {true,true,true,true,true,true},
+  {true,true,false,false,true,true},
+  {true,true,false,false,true,true},
+  {true,true,true,true,true,true},
+  {true,true,false,true,true,false},
+  {true,true,false,false,true,true}
+};
+
+bool miaumor_array[7][6][6];
+
 void setup() {
+  memcpy(miau_array[0], m_matrix, sizeof(m_matrix));
+  memcpy(miau_array[1], i_matrix, sizeof(i_matrix));
+  memcpy(miau_array[2], a_matrix, sizeof(a_matrix));
+  memcpy(miau_array[3], u_matrix, sizeof(u_matrix));
+
+  memcpy(miaumor_array[0], m_matrix, sizeof(m_matrix));
+  memcpy(miaumor_array[1], i_matrix, sizeof(i_matrix));
+  memcpy(miaumor_array[2], a_matrix, sizeof(a_matrix));
+  memcpy(miaumor_array[3], u_matrix, sizeof(u_matrix));
+  memcpy(miaumor_array[4], m_matrix, sizeof(m_matrix));
+  memcpy(miaumor_array[5], o_matrix, sizeof(o_matrix));
+  memcpy(miaumor_array[6], r_matrix, sizeof(r_matrix));
+
   // Set all pins conected to the led matrix to output
   // we only controll rows 2 to 7 and columns 2 to 7
   for(int i=2;i<14;i++){
@@ -25,43 +95,15 @@ void setup() {
 }
 
 void loop() {
-  bool m_matrix[6][6]={
-    {true,false,false,false,false,true},
-    {true,true,false,false,true,true},
-    {true,false,true,true,false,true},
-    {true,false,true,true,false,true},
-    {true,false,false,false,false,true},
-    {true,false,false,false,false,true}
-  };
-  sweepMatrix(m_matrix,sweeps);
-  
-  delay(delay_time);
-  bool u_matrix[6][6]={
-    {true,false,false,false,false,true},
-    {true,false,false,false,false,true},
-    {true,false,false,false,false,true},
-    {true,false,false,false,false,true},
-    {true,false,false,false,false,true},
-    {true,true,true,true,true,true}
-  };
-  sweepMatrix(u_matrix,sweeps);
-  delay(delay_time);
-  
-  bool a_matrix[6][6]={
-    {true,true,true,true,true,true},
-    {true,false,false,false,false,true},
-    {true,false,false,false,false,true},
-    {true,true,true,true,true,true},
-    {true,false,false,false,false,true},
-    {true,false,false,false,false,true}
-    
-  };
-  sweepMatrix(a_matrix,sweeps);
-  delay(delay_time);
+  //weepArrayOfMatrixes(miau_array, 4, 1500);
+  sweepArrayOfMatrixes(miaumor_array, 7, 1500);
 }
 
-void sweepMatrix(bool matrix[6][6],int sweeps){
-  for(int k=0;k<sweeps;k++){
+void sweepMatrix(bool matrix[6][6],int duration_milis){
+  // 1000(milliseg)/(execution time of this function) witch is aprox 290 micros
+  float frecuencia=3.5;
+  float sweeps=frecuencia*(float)duration_milis;
+  for(int k=0;k<(int)sweeps;k++){
     for(int i=0;i<6;i++){
       digitalWrite(transition_matrix[i][0][0],HIGH);
       for(int j=0;j<6;j++){
@@ -69,13 +111,19 @@ void sweepMatrix(bool matrix[6][6],int sweeps){
           digitalWrite(transition_matrix[i][j][1],LOW);
         }
       }
-      digitalWrite(transition_matrix[i][0][0],LOW);
       for(int j=0;j<6;j++){
         if(matrix[i][j]){
           digitalWrite(transition_matrix[i][j][1],HIGH);
         }
       }
-    }
+      digitalWrite(transition_matrix[i][0][0],LOW);
+    } 
   }
-  
+}
+
+void sweepArrayOfMatrixes(bool array[][6][6],int amount_element, int duration_ms ){
+  for(int i=0;i<amount_element;i++){
+    sweepMatrix(array[i], duration_ms);
+    delay(delay_time);
+  }
 }
