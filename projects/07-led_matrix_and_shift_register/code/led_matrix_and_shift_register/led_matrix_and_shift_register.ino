4edfa54767ea863cbec80662406dcca346cb1dfd
Scrocetti <sofia.17.crocetti@gmail.com>
2025-07-03 10:15:55 -0300

    FIX remove unnecesary code form the .ino file

--- projects/07-led_matrix_and_shift_register/code/led_matrix_and_shift_register/led_matrix_and_shift_register.ino
+++ projects/07-led_matrix_and_shift_register/code/led_matrix_and_shift_register/led_matrix_and_shift_register.ino
@@ -3,14 +3,13 @@
 #include "ShiftRegisterWriter.h"
 
 // === CONFIG ===
-constexpr bool COL_ACTIVE_LOW = true;    // columns active LOW (invert bits)
 constexpr uint8_t DATA_PIN  = 11;        // 74HC595 SER
 constexpr uint8_t CLOCK_PIN = 13;        // 74HC595 SRCLK
 constexpr uint8_t LATCH_PIN = 10;        // 74HC595 RCLK
 constexpr uint8_t ROW_PINS[8] = {2, 3, 4, 5, 6, 7, 8, 9}; // Matrix rows
 
 // Columns reset pattern (all off)
-constexpr uint8_t COLUMNS_RESETED = COL_ACTIVE_LOW ? 0xFF : 0x00;
+constexpr uint8_t COLUMNS_RESETED = 0xFF;
 
 // Create ShiftRegisterWriter instance with default MSBFIRST (ignored if you use dynamic write)
 ShiftRegisterWriter registerWriter(DATA_PIN, CLOCK_PIN, LATCH_PIN, MSBFIRST);
@@ -65,8 +64,7 @@ void drawPattern(const uint8_t* patternPtr, bool flipBits)
     uint8_t bits = readByte(patternPtr + row);
     if (!bits) continue;
 
-    uint8_t colData = COL_ACTIVE_LOW ? ~bits : bits;
-    if (flipBits) colData = reverse8(colData);
+    colData = reverse8(colData);
 
     digitalWrite(ROW_PINS[row], HIGH);
     registerWriter.write(colData, bitOrder);
