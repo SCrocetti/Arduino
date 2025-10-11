c907641d1cf7a665406066b6dea6609dee303101
Scrocetti <sofia.17.crocetti@gmail.com>
2025-10-11 20:42:44 -0300

    FEAT add testing code for the relay, still pending to test with a proper circuit

--- projects/02-relay_leds_and_tactile_switch/code/relay_leds_and_tactile_switch/relay_leds_and_tactile_switch.ino
+++ projects/02-relay_leds_and_tactile_switch/code/relay_leds_and_tactile_switch/relay_leds_and_tactile_switch.ino
@@ -1,5 +1,11 @@
+const int botonPin = 3;
+const int ledPin = 7;
 void setup() {
+  pinMode(botonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  int buttonState=digitalRead(botonPin);
+  digitalWrite(ledPin, buttonState);
 }
