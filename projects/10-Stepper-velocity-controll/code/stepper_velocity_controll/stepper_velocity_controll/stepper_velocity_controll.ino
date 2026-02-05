4c9e0b5ddff41eda02e61d03b6f3b93319e70cd4
Scrocetti <sofia.17.crocetti@gmail.com>
2026-02-05 13:12:05 -0300

    FIX steps and velocity calculation

--- projects/10-Stepper-velocity-controll/code/stepper_velocity_controll/stepper_velocity_controll/stepper_velocity_controll.ino
+++ projects/10-Stepper-velocity-controll/code/stepper_velocity_controll/stepper_velocity_controll/stepper_velocity_controll.ino
@@ -1,33 +1,37 @@
 #include <Stepper.h>
 
 // this is calculated with the datasheet
-const int stepsPerRevolution = 4096;
+const int stepsPerRevolution = 2048;
 const int maxSpeed = 5;
 
 // maximun  latency in ms, its the time it freezes to do the steps
 const int maxDelay=30;
-Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+Stepper myStepper(stepsPerRevolution, 8, 10, 9, 11);
 
 void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
   int pot = analogRead(A0);
-  int speed=0;
-  int direction=0;
-  if(pot==511 || pot==512){
-    return;
+
+  if (abs(pot - 512) < 5) return;
+
+  int direction;
+  int speed;
+
+  if (pot > 512) {
+    direction = -1;
+    speed = map(pot, 512, 1023, 0, maxSpeed);
+  } else {
+    direction = 1;
+    speed = map(pot, 511, 0, 0, maxSpeed);
   }
-  if(pot>512){
-    direction=-1;
-    speed=map(pot,512,1023,0,maxSpeed);
-  }
-  else if(pot<511){
-    direction=1;
-    speed=map(pot,0,511,0,maxSpeed);
-  }
+
+  if(speed==0)
+    return
 
   myStepper.setSpeed(speed);
   int steps=maxDelay*speed*stepsPerRevolution/60000;
-  myStepper.step(steps*direction); 
+  myStepper.step(steps*direction );
 }
